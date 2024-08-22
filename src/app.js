650fd3439856b8235f962d8d80e790770f3fcd4f
Alex Perez Rubio <71727357+alex5perez@users.noreply.github.com>
2024-08-22 20:10:14 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//Funcion para sacar una excusa aleatoria
+function showExcuseRandom() {
+    //Indice aleatorio de las frases
+    const index = Math.floor(Math.random() * excuses.length);
+    const excuseRandom = excuses[index];
+    //Cambiamos el texto con la id #excuse a una nueva excusa
+    document.getElementById("excuse").innerText = excuseRandom;
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//Array con las excusas
+const excuses = [
+    "Ayer estaba malo.",
+    "Me atracaron y me robaron los deberes.",
+    "Tuve una cita y no tuve tiempo.",
+    "No tenia internet.",
+    "Se cayeron las hojas en un cubo de agua."
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//Funcion para cuando recargamos la pagina window.onload
+window.onload = function(){
+    showExcuseRandom();
+}
